391130b186fb724d8a9d8a370e943a7c88e6aff6
SwanithReddy <swanithreddi@gmail.com>
2026-04-26 17:23:18 +0530

    added error handling step

--- FareCalc/farecalc.py
+++ FareCalc/farecalc.py
@@ -1,37 +1,31 @@
 print("===== Welcome to FareCalc  =====")
-rates = {
-    "economy": 12,
-    "premium": 17,
-    "suv": 26
-}
+dict={"economy": 12,"premium": 17,"suv": 26}
 
 
-def calc(km,vechile,hour):
-    rate=rates[vechile]
-    base=km*rate
-    surge=1
-    if 17<=hour<=20:
-        surge=1.5
-    total=base*surge
-    return rate,base,surge,total;
+def calc(km,type,hour):
+    if type not in dict:
+        return "service not available";
+    if hour>=17 and hour<=20:
+        price=km*dict[type]*1.5;
+        return price;
+    else:
+        return km*dict[type]
 
 
 km=float(input("Enter distance in km: "))
 vechile=input("Enter vechile type (Economy/Premium/SUV): ").lower()
 hour=int(input("Enter hour (0-23): "))
 
-if vechile in rates:
-    rate,basefare,surge,finalfare = calc(km,vechile,hour)
-    print("\n========== PRICE RECEIPT ==========")
-    print("Distance Travelled :", km, "km")
-    print("Vehicle Type       :", vechile.upper())
-    print("Rate per km        : ₹", rate)
-    print("Base Fare          : ₹", basefare)
-    print("Surge Multiplier   :", surge, "x")
-    print("Final Fare         : ₹", finalfare)
-    print("===================================")
+finalfare = calc(km,vechile,hour)
 
-else:
-    print("service unavailable")
+
+print("\n========== PRICE RECEIPT ==========")
+print("Distance Travelled :", km, "km")
+print("Vehicle Type       :", vechile.upper())
+print("Rate per km        : ₹", dict[vechile])
+print("Final Fare         : ₹", finalfare)
+print("===================================")
+
+
 
 
